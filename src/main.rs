bf1d7c7570fd405807245dbc0fb0bad39e86de47
Yuval Adam <_@yuv.al>
2022-05-12 20:47:23 +0300

    Build and insert into sqlite table, loop 10 times

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
+extern crate chrono;
 extern crate ping;
+extern crate sqlite;
 
+use std::thread;
 use std::time::{Duration,Instant};
+use chrono::prelude::*;
 
 fn main() {
-    let addr = "8.8.8.8".parse().unwrap();
+    let connection = sqlite::open(":memory:").unwrap();
+
+    connection.execute(
+        "
+        CREATE TABLE pings (ip TEXT, timestamp TEXT, ping REAL);
+        ",
+    ).unwrap();
+
+    let addrstr = "8.8.8.8";
+    let addr = addrstr.parse().unwrap();
     let timeout = Duration::from_secs(1);
-    let now = Instant::now();
-    ping::ping(addr, Some(timeout), Some(166), Some(3), Some(5), None).unwrap();
-    let new_now = Instant::now();
-    println!("{:?}", new_now.duration_since(now));
+
+    for i in 1..10 {
+        let now = Instant::now();
+        ping::ping(addr, Some(timeout), Some(166), Some(3), Some(i), None).unwrap();
+        let new_now = Instant::now();
+        let local: DateTime<Local> = Local::now();
+
+        connection.execute(
+            format!("INSERT INTO pings VALUES ('{}', '{}', {});", addrstr, local, new_now.duration_since(now).as_micros()),
+        ).unwrap();
+        println!("{} {} {}", addrstr, local, new_now.duration_since(now).as_micros());
+        thread::sleep(Duration::from_secs(1));
+    }
+    
 }
