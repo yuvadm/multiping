fc453b7413fdb598791d30e88e6c7b7571b34693
Yuval Adam <_@yuv.al>
2022-05-12 19:59:24 +0300

    Implement basic ping with timing

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Multiping
+
+## Usage
+
+Since multiping uses raw ICMP socks it has to be run with elevated privileges:
+
+```bash
+$ cargo build
+$ sudo ./target/debug/multiping
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate ping;
+
+use std::time::{Duration,Instant};
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "8.8.8.8".parse().unwrap();
+    let timeout = Duration::from_secs(1);
+    let now = Instant::now();
+    ping::ping(addr, Some(timeout), Some(166), Some(3), Some(5), None).unwrap();
+    let new_now = Instant::now();
+    println!("{:?}", new_now.duration_since(now));
 }
